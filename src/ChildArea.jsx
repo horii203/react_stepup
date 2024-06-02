e53dc6568663f227709b668e1f79608b7c6c3168
horii203 <89522294+horii203@users.noreply.github.com>
2024-06-02 17:05:03 +0900

    レンダリング最適化1

--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -1,11 +1,20 @@
+import { memo } from "react";
 const style = {
   with: "100%",
   height: "200px",
   backgroundColor: "khaki",
 };
 
-export const ChildArea = (props) => {
+// memo:propsに変更がない限り再レンダリングしない
+export const ChildArea = memo((props) => {
   const { open } = props;
+
+  //ダミー処理
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("///");
+  });
+
   return (
     <>
       {/* 三項演算子。openがtrueだったときだけ表示 */}
@@ -16,4 +25,4 @@ export const ChildArea = (props) => {
       ) : null}
     </>
   );
-};
+});
